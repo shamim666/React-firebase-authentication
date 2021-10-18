764ef03d81702b71205a9f77778619630a61a9f3
shamim <shamim.pstu@gmail.com>
2021-10-18 17:14:58 +0600

    firebase

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import { getAuth, signInWithPopup,GoogleAuthProvider } from "firebase/auth";
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 import initializeAuthentication from './Firebase/firebase.init';
 
 
@@ -10,28 +10,32 @@ const provider = new GoogleAuthProvider();
 function App() {
   const auth = getAuth();
 
-  const handleGoogleSignIn = () =>{
+  const handleGoogleSignIn = () => {
 
-signInWithPopup(auth, provider)
-.then(result =>{
-  const user = result.user;
-  console.log(user);
-})
+    signInWithPopup(auth, provider)
+      .then(result => {
+        const user = result.user;
+        console.log(user);
+      })
+  }
 
+  const handleRegistration = (event) => {
+    console.log("Added")
 
+    event.preventDefault();
   }
   return (
     <div className="App">
-      <form >
+      <form onSubmit={handleRegistration}>
 
-      <h3>Please Register</h3>
-      <label htmlFor="Email">Email:</label>
-      <input type="text" name="email" />
-      <br />
-      <label htmlFor="password">Password:</label>
-      <input type="password" name="password" id="" />
-      <br />
-      <input type="button" value="Submit" />
+        <h3>Please Register</h3>
+        <label htmlFor="Email">Email:</label>
+        <input type="text" name="email" />
+        <br />
+        <label htmlFor="password">Password:</label>
+        <input type="password" name="password" id="" />
+        <br />
+        <input type="submit" value="Register" />
       </form>
       <div>------------------------------</div>
       <button onClick={handleGoogleSignIn}>Google Sign In</button>
