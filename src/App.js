cfb7efccf4fcecb910b7604493732663ea30ab71
shamim <shamim.pstu@gmail.com>
2021-10-18 16:59:17 +0600

    firebase

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
+import { getAuth, signInWithPopup,GoogleAuthProvider } from "firebase/auth";
+import initializeAuthentication from './Firebase/firebase.init';
+
+
+initializeAuthentication();
+const provider = new GoogleAuthProvider();
 
 function App() {
+  const auth = getAuth();
+
+  const handleGoogleSignIn = () =>{
+
+signInWithPopup(auth, provider)
+.then(result =>{
+  const user = result.user;
+  console.log(user);
+})
+
+
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form >
+
+      <h3>Please Register</h3>
+      <label htmlFor="Email">Email:</label>
+      <input type="text" name="email" />
+      <br />
+      <label htmlFor="password">Password:</label>
+      <input type="password" name="password" id="" />
+      <br />
+      <input type="button" value="Submit" />
+      </form>
+      <div>------------------------------</div>
+      <button onClick={handleGoogleSignIn}>Google Sign In</button>
+
     </div>
   );
 }
---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyC6EWXcdL45_o2fQnwObiNCUS5mtGX60tU",
+    authDomain: "simple-firebase-authenti-cf020.firebaseapp.com",
+    projectId: "simple-firebase-authenti-cf020",
+    storageBucket: "simple-firebase-authenti-cf020.appspot.com",
+    messagingSenderId: "1074196453024",
+    appId: "1:1074196453024:web:722511f49a3bdfee8b1bc5"
+  };
+
+  export default firebaseConfig ; 
